154ad516f6764f3c2d5d415fdc3c3ce3b26c7eaa
smeerws <hello@smeerws.at>
2019-06-04 08:43:58 +0200

    minor fixes format

--- Assets/MyGame/Scripts/Player.cs
+++ Assets/MyGame/Scripts/Player.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour {
-
+public class Player : MonoBehaviour
+{
 	private const string AXISHORIZONTAL = "Horizontal";
 	private float moveSpeed = 3.0f;
 
@@ -15,8 +13,8 @@ public class Player : MonoBehaviour {
         transform.position = new Vector2(newPosX, transform.position.y);
     }
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		Move();
 	}
 }
